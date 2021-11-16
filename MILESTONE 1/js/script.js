e51f06152f0e331f58a62b1e900603428cc841e1
Darius104 <comandarius@gmail.com>
2021-11-16 17:31:35 +0100

    js done

--- MILESTONE 1/js/script.js
+++ MILESTONE 1/js/script.js
@@ -1,17 +1,3 @@
-// const burgerButton = document.querySelector('.header-right > a');
-// burgerButton.addEventListener('click',
-//     function() {
-//         const hamburgerMenu = document.querySelector('.hamburger-menu');
-//         hamburgerMenu.classList.add('active');
-//     }
-// );
-// const closeButton = document.querySelector('.close');
-// closeButton.addEventListener('click',
-//     function() {
-//         const hamburgerMenu = document.querySelector('.hamburger-menu');
-//         hamburgerMenu.classList.remove('active');
-//     }
-// );
 for( let z = 1; z <= 100; z++ ){
     
     if( z % 3 === 0 && z % 5 === 0){

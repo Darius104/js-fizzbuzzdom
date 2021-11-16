a6654a9cd611733a3416c099127e6f01afd2c430
Darius104 <comandarius@gmail.com>
2021-11-16 16:25:29 +0100

    Milestone 1

---
+++ MILESTONE 1/js/script.js
@@ -0,0 +1,35 @@
+// const burgerButton = document.querySelector('.header-right > a');
+// burgerButton.addEventListener('click',
+//     function() {
+//         const hamburgerMenu = document.querySelector('.hamburger-menu');
+//         hamburgerMenu.classList.add('active');
+//     }
+// );
+// const closeButton = document.querySelector('.close');
+// closeButton.addEventListener('click',
+//     function() {
+//         const hamburgerMenu = document.querySelector('.hamburger-menu');
+//         hamburgerMenu.classList.remove('active');
+//     }
+// );
+for( let z = 1; z <= 100; z++ ){
+
+    if ( z % 3 == 0 && z % 5 == 0 ){
+        console.log('FizzBuzz');
+        z = z + 1;
+
+    }
+    if ( z % 3 == 0 ){
+        console.log('Fizz');
+        z = z + 1;
+
+    }
+    if ( z % 5 == 0 ){
+        console.log('Buzz');
+        z = z + 1;
+
+    }
+    
+    console.log(z);
+}
+
